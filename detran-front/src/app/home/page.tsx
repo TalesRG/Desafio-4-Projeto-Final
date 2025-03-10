df0f8064e00bc8b038a990f9a021cae5de8aa1cb
BrunoBReis <brunobragancadosreis@gmail.com>
2025-03-10 20:03:21 -0300

    feat: add DashboardPage component with card links for navigation

---
+++ detran-front/src/app/home/page.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import React from "react";
+import CardLink from "@/app/ui/home/card-link";
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold pb-10 text-center md:text-left">
+        Bem-vindo Username
+      </h1>
+      <div className="flex flex-col md:flex-row justify-center md:justify-start gap-x-6 gap-y-10">
+        {/* Carro Card */}
+        <CardLink
+          href="/home/carro"
+          iconSrc="/car-side.svg"
+          altText="Car Icon"
+          title="Carro"
+          description="Para acessar as informações do seu carro."
+        />
+
+        {/* Multa Card */}
+        <CardLink
+          href="/home/multa"
+          iconSrc="/multa.svg"
+          altText="Ticket Icon"
+          title="Multa"
+          description="Veja suas multas pendentes e pague online."
+        />
+
+        {/* Proprietário Card */}
+        <CardLink
+          href="/home/proprietario"
+          iconSrc="/proprietario.svg"
+          altText="Owner Icon"
+          title="Proprietário"
+          description="Gerencie seus dados de proprietário do veículo."
+        />
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
