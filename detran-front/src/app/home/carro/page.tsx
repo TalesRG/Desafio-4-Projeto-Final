9ea29260f514d4070776f779ff4b19ddf458827d
BrunoBReis <brunobragancadosreis@gmail.com>
2025-03-10 20:03:28 -0300

    feat: add HomePage component with title for car table

---
+++ detran-front/src/app/home/carro/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+    return (
+        <div className="flex items-start justify-center">
+            <h1 className="text-4xl font-bold text-blue-500">Tabela dos Carros</h1>
+        </div>
+    );
+};
+
+export default HomePage;
