d0f53bf5aa5d57811efd60b5fc431be1a380cbff
BrunoBReis <brunobragancadosreis@gmail.com>
2025-03-11 22:42:40 -0300

    feat: refactor vehicle owner registration form with new components and input enhancements

--- detran-front/src/app/home/proprietario/cadastro/page.tsx
+++ detran-front/src/app/home/proprietario/cadastro/page.tsx
@@ -3,8 +3,12 @@
 import React, { useState } from 'react';
 import GenericInput from '@/app/ui/generic-form';
 import Button from '@/app/ui/button';
-import {ProprietarioRegister} from "@/type/ProprietarioRegister";
-import {cadastrarProprietario} from "@/service/proprietarioService";
+import CpfButton from '@/app/ui/home/proprietario/cadastro/cpf-button';
+import PhoneButton from '@/app/ui/home/proprietario/cadastro/phone-button';
+import StatesButton from '@/app/ui/home/proprietario/cadastro/states-button';
+import SexButton from '@/app/ui/home/proprietario/cadastro/sex-button';
+import { ProprietarioRegister } from "@/type/ProprietarioRegister";
+import { cadastrarProprietario } from "@/service/proprietarioService";
 
 export default function CadastroProprietario() {
   const [nome, setNome] = useState('');
@@ -18,6 +22,7 @@ export default function CadastroProprietario() {
   const [sexo, setSexo] = useState('');
   const [dataNascimento, setDataNascimento] = useState<Date | null>(null);
   const [pontosNaCarteira, setPontosNaCarteira] = useState<number>(0);
+  const [observacoes, setObservacoes] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
   const handleDateChange = (dateStr: string) => {
@@ -44,98 +49,104 @@ export default function CadastroProprietario() {
     try {
       await cadastrarProprietario(data);
       setErrorMessage('');
-    }catch (e) {
+    } catch (e) {
       console.error(e);
       setErrorMessage('Erro ao cadastrar proprietário.');
     }
   };
 
   return (
-      <div className="min-w-60 max-w-xl mx-auto p-0 md:p-10 h-full overflow-auto">
-        <h1 className="text-3xl font-bold mb-6">Cadastro de Proprietários</h1>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <GenericInput
-              title="Nome"
-              pattern=".+"
-              value={nome}
-              errorMessage="Nome inválido."
-              onChange={(value) => setNome(value)}
-          />
-          <GenericInput
-              title="CPF"
-              pattern="^\\d{11}$"
-              value={cpf}
-              placeholder="Somente números"
-              errorMessage="CPF inválido. Deve conter 11 dígitos."
-              onChange={(value) => setCpf(value)}
-          />
-          <GenericInput
-              title="E-mail"
-              pattern="^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$"
-              value={email}
-              errorMessage="E-mail inválido."
-              onChange={(value) => setEmail(value)}
-          />
-          <GenericInput
-              title="Endereço"
-              pattern=".+"
-              value={endereco}
-              errorMessage="Endereço inválido."
-              onChange={(value) => setEndereco(value)}
-          />
-          <GenericInput
-              title="Bairro"
-              pattern=".+"
-              value={bairro}
-              errorMessage="Bairro inválido."
-              onChange={(value) => setBairro(value)}
-          />
-          <GenericInput
-              title="Cidade"
-              pattern=".+"
-              value={cidade}
-              errorMessage="Cidade inválida."
-              onChange={(value) => setCidade(value)}
-          />
-          <GenericInput
-              title="Telefone"
-              pattern="^\d{10,11}$"
-              value={telefone}
-              placeholder="Somente números"
-              errorMessage="Telefone inválido."
-              onChange={(value) => setTelefone(value)}
-          />
-          <GenericInput
-              title="Estado"
-              pattern=".+"
-              value={estado}
-              errorMessage="Estado inválido."
-              onChange={(value) => setEstado(value)}
-          />
-          <GenericInput
-              title="Sexo"
-              pattern=".+"
-              value={sexo}
-              errorMessage="Sexo inválido."
-              onChange={(value) => setSexo(value)}
-          />
-          <GenericInput
-              title="Data de Nascimento"
-              pattern="^\d{4}-\d{2}-\d{2}$"
-              value={dataNascimento ? dataNascimento.toISOString().slice(0, 10) : ''}
-              type="date"
-              errorMessage="Data de nascimento inválida."
-              onChange={(value) => handleDateChange(value)}
-          />
-          <GenericInput
-              title="Pontos na Carteira"
-              pattern="^\d+$"
-              value={pontosNaCarteira.toString()}
-              errorMessage="Deve ser um número válido."
-              onChange={(value) => setPontosNaCarteira(Number(value))}
-          />
-          <Button title="Cadastro"/>
-        </form>
-      </div>
+    <div className="min-w-60 max-w-xl mx-auto p-0 md:p-10 h-full overflow-auto">
+      <h1 className="text-3xl font-bold mb-6">Cadastro de Proprietários</h1>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <GenericInput
+          title="Nome"
+          pattern=".+"
+          value={nome}
+          errorMessage="Nome inválido."
+          placeholder="Nome Completo"
+          maxLength={51} // Levado em consideração o maior nome do brasil
+          onChange={(value) => setNome(value)}
+        />
+        <CpfButton
+          value={cpf}
+          onChange={(value) => setCpf(value)}
+        />
+        <GenericInput
+          title="E-mail"
+          pattern="^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$"
+          value={email}
+          errorMessage="E-mail inválido."
+          placeholder="E-mail"
+          maxLength={320} // Levado em consideração o limite
+          onChange={(value) => setEmail(value)}
+        />
+        <GenericInput
+          title="Endereço"
+          pattern=".+"
+          value={endereco}
+          errorMessage="Endereço inválido."
+          placeholder="Endereço"
+          maxLength={100}
+          onChange={(value) => setEndereco(value)}
+        />
+        <GenericInput
+          title="Bairro"
+          pattern=".+"
+          value={bairro}
+          errorMessage="Bairro inválido."
+          placeholder="Bairro"
+          maxLength={50}
+          onChange={(value) => setBairro(value)}
+        />
+        <GenericInput
+          title="Cidade"
+          pattern=".+"
+          value={cidade}
+          errorMessage="Cidade inválida."
+          placeholder="Cidade"
+          maxLength={50}
+          onChange={(value) => setCidade(value)}
+        />
+        <PhoneButton
+          value={telefone}
+          onChange={(value) => setTelefone(value)}
+        />
+        <StatesButton
+          value={estado}
+          onChange={(value) => setEstado(value)}
+        />
+        <SexButton 
+          value={sexo}
+          onChange={(value) => setSexo(value)}
+        />
+        <GenericInput
+          title="Data de Nascimento"
+          pattern="^\d{4}-\d{2}-\d{2}$"
+          value={dataNascimento ? dataNascimento.toISOString().slice(0, 10) : ''}
+          type="date"
+          errorMessage="Data de nascimento inválida."
+          onChange={(value) => handleDateChange(value)}
+        />
+        <GenericInput
+          title="Pontos na Carteira"
+          pattern="^\d+$"
+          value={pontosNaCarteira.toString()}
+          errorMessage="Deve ser um número válido."
+          maxLength={2}
+          onChange={(value) => setPontosNaCarteira(Number(value))}
+        />
+        <GenericInput
+          title="Observações"
+          pattern=".*"
+          value={observacoes}
+          errorMessage="Limite de 50 caracteres."
+          placeholder="Observações"
+          maxLength={100}
+          onChange={(value) => setCidade(value)}
+        />
+        <Button title="Cadastro" />
+      </form>
+    </div>
   );
 }
